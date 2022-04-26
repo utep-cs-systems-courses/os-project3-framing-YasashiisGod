a715a1f184f4f2bdac192e873df1734f7c924cef
brianperez <secretlyytheflash@gmail.com>
2022-04-26 11:50:08 -0600

    Client to server messages A-OK

--- lab/server4Lab.py
+++ lab/server4Lab.py
@@ -1,29 +1,45 @@
 import socket
 import threading
 
+HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 
-def handle_client(conn, addr): #handle each connection
+def handle_client(conn, addr):  # handle each connection
     print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
     while connected:
-        msg = conn.recv()
-        "hello000000000000"
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
 
-def start(): #handle new connections
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}]{msg}")
+
+    conn.close()
+
+
+def start():  # handle new connections
     server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept() #blocks and waits for new connection to the server and store the connection and address
-        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        conn, addr = server.accept()  # blocks and waits for new connection to
+        # the server and store the connection and address
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
 
 print("[STARTING] server is starting")
 start()
